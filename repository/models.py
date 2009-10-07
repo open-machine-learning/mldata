dbc0f1c202901428f8d6eb0f3bf63f5576b82f87
Sebastian Henschel <shogun@kodeaffe.de>
2009-10-07 15:38:05 +0000

    - adjusted models for repository after last meeting.

--- repository/models.py
+++ repository/models.py
@@ -1,22 +1,31 @@
 from django.db import models
+from utils import slugify
+import tagging
 
 
 class Repository(models.Model):
     pub_date = models.DateTimeField()
-    mod_date = models.DateTimeField()
     version = models.IntegerField()
+    name = models.CharField(max_length=32)
+    slug = models.CharField(max_length=32) # meta id
     summary = models.CharField(max_length=255)
     description = models.TextField()
     urls = models.CharField(max_length=255)
     publications = models.CharField(max_length=255)
     license = models.CharField(max_length=255)
+    is_public = models.BooleanField()
 
     class Meta:
         ordering = ('-pub_date',)
         get_latest_by = 'pub_date'
 
     def __unicode__(self):
-        return unicode(self.summary)
+        return unicode(self.name)
+
+    def save(self):
+        if not self.id:
+            self.slug = slugify(self.name)
+        super(Repository, self).save()
 
 
 class Data(Repository):
@@ -24,16 +33,17 @@ class Data(Repository):
     format = models.CharField(max_length=16) # CSV, ARFF, netCDF, HDF5, ODBC
     measurement_details = models.TextField()
     usage_scenario = models.TextField()
+    file = models.FileField(upload_to='repository/data')
 
     def get_absolute_url(self):
-        modulator = 16384
-        return "/repository/data/%s/%s/" % (self.id % modulator, self.id)
+        return self.file.url
+
+tagging.register(Data)
 
 
 class Task(Repository):
     format_input = models.CharField(max_length=255)
     format_output = models.CharField(max_length=255)
-    training_test_offset = models.IntegerField()
     performance_measure = models.CharField(max_length=255)
     data = models.ManyToManyField(Data)
 
@@ -43,6 +53,15 @@ class Solution(Repository):
     parameters = models.CharField(max_length=255)
     os = models.CharField(max_length=255)
     code = models.TextField()
-    score = models.CharField(max_length=255)
+    score = models.FileField(upload_to='repository/scores')
     task = models.ForeignKey(Task)
+
+
+class Split(models.Model):
+    data = models.ForeignKey(Data)
+    task = models.ForeignKey(Task)
+    splits = models.FileField(upload_to='repository/splits')
+
+    def get_absolute_url(self):
+        return self.splits.url
 
