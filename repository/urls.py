7d3a1a243d91fabcc2395e2d63ebbc64ac0c6669
Sebastian Henschel <shogun@kodeaffe.de>
2009-10-08 17:24:28 +0000

    - created some views and templates for Data, Task, Solution in repository.

--- repository/urls.py
+++ repository/urls.py
@@ -3,5 +3,10 @@ import repository.views as views
 
 urlpatterns = patterns('',
     (r'^$', views.index),
-    (r'^data/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'data'}),
+    (r'^data/$', views.data_index),
+    (r'^data/new$', views.data_new),
+    (r'^task/$', views.task_index),
+    (r'^task/new$', views.task_new),
+    (r'^solution/$', views.solution_index),
+    (r'^solution/new$', views.solution_new),
 )
--- repository/views.py
+++ repository/views.py
@@ -8,3 +8,25 @@ from repository.models import Data, Task, Solution
 def index(request):
 	return render_to_response('repository/index.html')
 
+
+def data_index(request):
+	return render_to_response('repository/data_index.html')
+
+def data_new(request):
+	return render_to_response('repository/data_new.html')
+
+
+def task_index(request):
+	return render_to_response('repository/task_index.html')
+
+def task_new(request):
+	return render_to_response('repository/task_new.html')
+
+
+def solution_index(request):
+	return render_to_response('repository/solution_index.html')
+
+def solution_new(request):
+	return render_to_response('repository/solution_new.html')
+
+
