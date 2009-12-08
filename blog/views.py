8efa8f4ff6ed8a76b459a17a03df62fe06d9e31a
Sebastian Henschel <shogun@kodeaffe.de>
2009-12-08 10:28:54 +0000

    - added preview functionality to blog posts.

--- blog/views.py
+++ blog/views.py
@@ -18,19 +18,28 @@ def new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            post = Post()
-            post.pub_date = datetime.datetime.now()
-            post.headline = form.cleaned_data['headline']
-            post.summary = form.cleaned_data['summary']
-            post.body = form.cleaned_data['body']
-            post.author_id = request.user.id
-            post.save()
-            return HttpResponseRedirect(post.get_absolute_url())
+            if hasattr(request.POST, 'preview'):
+                preview = {
+                    'headline': form.cleaned_data['headline'],
+                    'summary': form.cleaned_data['summary'],
+                    'body': form.cleaned_data['body'],
+                }
+            else:
+                post = Post()
+                post.pub_date = datetime.datetime.now()
+                post.headline = form.cleaned_data['headline']
+                post.summary = form.cleaned_data['summary']
+                post.body = form.cleaned_data['body']
+                post.author_id = request.user.id
+                post.save()
+                return HttpResponseRedirect(post.get_absolute_url())
     else:
+        preview = False
         form = PostForm()
 
     info_dict = {
         'user': request.user,
         'form': form,
+        'preview': preview,
     }
     return render_to_response('blog/new.html', info_dict)
