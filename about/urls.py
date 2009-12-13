4926ddc173cb11f057fcbec40e95d935aaba7d85
Sebastian Henschel <shogun@kodeaffe.de>
2009-12-13 12:18:25 +0000

    - added page for related projects, improved structure of about section.

--- about/urls.py
+++ about/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
-import about.views as views
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='about_index'),
-    (r'^about/$', views.index),
-    (r'^impressum/$', views.impressum),
-    (r'^tos/$', views.tos),
+    url(r'^$', direct_to_template, {'template':'about/index.html'}, name='about_index'),
+    url(r'^impressum/$', direct_to_template, {'template':'about/impressum.html'}, name='about_impressum'),
+    url(r'^tos/$', direct_to_template, {'template':'about/tos.html'}, name='about_tos'),
+    url(r'^related/$', direct_to_template, {'template':'about/related.html'}, name='about_related'),
 )
