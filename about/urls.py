8fb38d15593b6868927f1a2f9cc619d33348faf2
Sebastian Henschel <shogun@kodeaffe.de>
2010-01-11 17:39:26 +0000

    - introduced new webdesign, starting with section about.

--- about/urls.py
+++ about/urls.py
@@ -5,9 +5,19 @@ URLconf for app About
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
+extra_context = { 'section': 'about' }
+
 urlpatterns = patterns('',
-    url(r'^$', direct_to_template, {'template':'about/index.html'}, name='about_index'),
-    url(r'^impressum/$', direct_to_template, {'template':'about/impressum.html'}, name='about_impressum'),
-    url(r'^tos/$', direct_to_template, {'template':'about/tos.html'}, name='about_tos'),
-    url(r'^related/$', direct_to_template, {'template':'about/related.html'}, name='about_related'),
+    url(r'^$', direct_to_template,
+        {'template':'about/index.html', 'extra_context':extra_context},
+        name='about_index'),
+    url(r'^impressum/$', direct_to_template,
+        {'template':'about/impressum.html', 'extra_context':extra_context},
+        name='about_impressum'),
+    url(r'^tos/$', direct_to_template,
+        {'template':'about/tos.html', 'extra_context':extra_context},
+        name='about_tos'),
+    url(r'^related/$', direct_to_template,
+        {'template':'about/related.html', 'extra_context':extra_context},
+        name='about_related'),
 )
