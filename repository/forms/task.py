e3e93bb6e12aa9d817110f5c52fca21a539b3834
Hagen Zahn <hzahn@informatik.hu-berlin.de>
2010-10-05 13:29:18 +0000

    change input/output format for datasplit

--- repository/forms/task.py
+++ repository/forms/task.py
@@ -7,6 +7,7 @@ from django.utils.translation import ugettext as _
 from django.http import HttpResponseRedirect
 
 import ml2h5.task
+from ml2h5.indexsplit import expand_split_str,check_split_str
 from  mleval import evaluation
 
 from settings import TAG_SPLITSTR
@@ -104,8 +105,8 @@ class TaskForm(RepositoryForm):
 
 
 
-    def _clean_commaseplistofintegers(self, name):
-        """Ensure field with given name is a comma-seperated list of integers.
+    def _clean_valid_inputformat(self, name):
+        """Ensure field with given name has a valid format.
 
         @param name: name of field to clean
         @type name: string
@@ -115,20 +116,20 @@ class TaskForm(RepositoryForm):
         if name in self.cleaned_data:
             if not self.cleaned_data[name]:
                 return None
-            try:
-                return [int(x) for x in self.cleaned_data[name].split(',')]
-            except:
-                raise ValidationError(_('Not a comma-seperated list of integers.'))
+	    if check_split_str(self.cleaned_data[name]): 
+                return [int(x) for x in expand_split_str(self.cleaned_data[name])]
+	    else:
+		    raise ValidationError(_('Invalid Format (smpl: 1,2,3:5).'))
 
     def clean_train_idx(self):
         """Ensure train_idx are given as comma-seperated list of integers."""
-        return self._clean_commaseplistofintegers('train_idx')
+        return self._clean_valid_inputformat('train_idx')
     def clean_test_idx(self):
         """Ensure test_idx are given as comma-seperated list of integers."""
-        return self._clean_commaseplistofintegers('test_idx')
+        return self._clean_valid_inputformat('test_idx')
     def clean_input_variables(self):
         """Ensure input variables are given as comma-seperated list of integers."""
-        return self._clean_commaseplistofintegers('input_variables')
+        return self._clean_valid_inputformat('input_variables')
 
 
     def clean_output_variables(self):
