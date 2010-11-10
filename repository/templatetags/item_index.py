0c8fdc751e1ca016ea38f250687f14b873c911c5
Soeren Sonnenburg <sonne@debian.org>
2010-11-10 11:20:57 +0000

    add missing file

---
+++ repository/templatetags/item_index.py
@@ -0,0 +1,20 @@
+from django import template
+from urllib import quote
+
+register = template.Library()
+@register.inclusion_tag('repository/item_index_common.html', takes_context=True)
+
+def item_index(context, queryset, klass):
+    pname=klass.lower() + '_page'
+    r= {
+            'klass': klass,
+            pname: queryset,
+            'page': queryset,
+            'page_name': pname,
+        }
+    if context.has_key('searchterm'):
+        r['searchterm']=quote(context['searchterm'],'')
+        r['selecttab']='#tabs-' + klass.lower()
+        if context.has_key('klass'):
+            r['klass']=quote(context['klass'],'')
+    return r
