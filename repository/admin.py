d76f784dd3b6330cea0c7cde04e97dff674b9ec8
Soeren Sonnenburg <sonne@debian.org>
2010-11-19 14:05:01 +0000

    improve admin display

--- repository/admin.py
+++ repository/admin.py
@@ -7,11 +7,10 @@ from repository.models import *
 
 class DataAdmin(admin.ModelAdmin):
     """Admin class for Data"""
-    list_display = ('name', 'pub_date', 'slug', 'user', 'is_public', 'file', 'format')
+    list_display = ('name', 'version', 'pub_date', 'slug', 'user', 'is_public', 'is_current', 'file', 'format')
     date_hierarchy = 'pub_date'
-    list_filter =['pub_date', 'user', 'is_public', 'tags', 'format']
+    list_filter =['pub_date', 'user', 'is_public', 'is_current', 'tags', 'format']
     search_fields = ['name','file']
-
 admin.site.register(Data, DataAdmin)
 
 class SlugAdmin(admin.ModelAdmin):
@@ -21,20 +20,32 @@ admin.site.register(Slug, SlugAdmin)
 
 class SolutionAdmin(admin.ModelAdmin):
     """Admin class for Solution"""
-    list_display = ('name', 'pub_date', 'slug', 'is_public',)
+    list_display = ('name', 'version', 'pub_date', 'slug', 'is_public', 'is_current')
+    date_hierarchy = 'pub_date'
+    list_filter =['pub_date', 'user', 'is_public', 'is_current', 'tags']
+    search_fields = ['name']
 admin.site.register(Solution, SolutionAdmin)
 
 class ResultAdmin(admin.ModelAdmin):
     """Admin class for Result"""
     list_display = ('solution', 'task', 'challenge', 'aggregation_score', 'pub_date',)
+    date_hierarchy = 'pub_date'
+    list_filter =['solution', 'task', 'challenge']
+    search_fields = ['name']
 admin.site.register(Result, ResultAdmin)
 
 class TaskAdmin(admin.ModelAdmin):
     """Admin class for Task"""
-    list_display = ('name', 'pub_date', 'slug', 'is_public')
+    list_display = ('name', 'version', 'pub_date', 'slug', 'is_public', 'is_current')
+    date_hierarchy = 'pub_date'
+    list_filter =['pub_date', 'user', 'is_public', 'is_current', 'tags']
+    search_fields = ['name']
 admin.site.register(Task, TaskAdmin)
 
 class ChallengeAdmin(admin.ModelAdmin):
     """Admin class for Challenge"""
-    list_display = ('name', 'pub_date', 'slug', 'is_public')
+    list_display = ('name', 'version', 'pub_date', 'slug', 'is_public', 'is_current')
+    date_hierarchy = 'pub_date'
+    list_filter =['pub_date', 'user', 'is_public', 'is_current', 'tags']
+    search_fields = ['name']
 admin.site.register(Challenge, ChallengeAdmin)
