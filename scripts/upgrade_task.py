f923f1405ec9e0a0b5f067e141af3813a0fc76bc
Soeren Sonnenburg <sonne@debian.org>
2011-03-10 15:19:19 +0000

    script we used to convert task file to new format

--- scripts/upgrade_task.py
+++ scripts/upgrade_task.py
@@ -8,16 +8,29 @@ for t in Task.objects.all():
     #s='%s\t%s\t%s' % (d.format, d.file.name, '%s_v%d%s' % (d.slug, d.version, ext))
     #dst=os.path.join(MEDIA_ROOT,'%s_v%d%s' % (d.slug, d.version, ext))
     src1=os.path.join(MEDIA_ROOT, t.file.name)
+    if src1=='/home/mldata/private/task/mkl-toy.txt':
+        continue
     if os.path.exists(src1):
-        h5=h5py.File(src1,'r')
+        print t, src1
+        h5=h5py.File(src1,'r+')
         taskinfo={}
-        for field in list(h5['task']):
-            taskinfo[field]=h5['task'][field][...]
-        if taskinfo.has_key('train_idx'):
-            if not taskinfo.has_key('val_idx'): 
-                taskinfo['val_idx']=[]        
-            if not taskinfo.has_key('test_idx'): 
-                taskinfo['test_idx']=[]        
+        for field in ('train_idx', 'val_idx', 'test_idx'):
+            print field
+            taskinfo[field]=[]        
+            if field in h5['task'].keys():
+                x=h5['task'][field][...]
+                if x.ndim==1:
+                   x=x.reshape(1,len(x))
+                else:
+                   x=x.transpose()
+                y=list()
+                for l in x:
+                    y.append([', '.join([str(i) for i in l])])
+                
+                taskinfo[field]=y
+
+        if len(taskinfo['train_idx'])>0:
+            print t.data, t.data.num_instances
             data_size=t.data.num_instances
             taskinfo['data_split']=conv_idx2image(taskinfo['train_idx'],taskinfo['val_idx'],taskinfo['test_idx'],data_size)
             taskinfo['data_size']=data_size
@@ -25,4 +38,12 @@ for t in Task.objects.all():
             del taskinfo['val_idx']
             del taskinfo['test_idx']
             update_data(h5,taskinfo)
+
+        for field in ('train_idx', 'val_idx', 'test_idx'):
+            try:
+                del h5['task'][field]
+            except:
+                pass
+
         h5.close()        
+        print
