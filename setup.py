3d52bced2ec81baf739abb7683b6fb4c2f8db091
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2011-08-19 10:33:21 +0200

    Initial fixtures for repository and datasets download by python script

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import tarfile
+import os
+
+src = "http://lemonet.eu/static/"
+out_dir = "media/"
+file_name = "private.tar.gz"
+
+def download(url):
+    """Copy the contents of a file from a given URL
+    to a local file.
+    """
+    import urllib
+    webFile = urllib.urlopen(url)
+    localFile = open(out_dir + file_name, 'w')
+    localFile.write(webFile.read())
+    webFile.close()
+    localFile.close()
+
+def install():
+    try:
+        print 'Downloading %s... ' % (file_name,)
+        download(src + file_name)
+    except IOError:
+        print "IOError"
+        return
+        
+    print 'Extracting %s... ' % (file_name,)
+    tfile = tarfile.open(out_dir + file_name, 'r:gz')
+    tfile.extractall('media')
+    print 'Removing %s... ' % (file_name,)
+    os.remove(out_dir + file_name)
+
+    print "Done!"
+
+if __name__ == '__main__':
+    import sys
+    if sys.argv[1] == "install":
+        install()
+    else:
+        import os
+        print 'usage: python %s install' % os.path.basename(sys.argv[0])
