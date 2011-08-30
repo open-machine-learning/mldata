c72d2230665502ac74365d63df4a92c3dbcb46b8
Łukasz Kidziński <lukasz.kidzinski@gmail.com>
2011-08-30 12:28:06 +0200

    Captcha repaired, placeholders for captcha keys added to settings.py

--- captcha/fields.py
+++ captcha/fields.py
@@ -3,13 +3,10 @@ from django import forms
 from django.utils.encoding import smart_unicode
 from django.utils.translation import ugettext_lazy as _
 
+from captcha.widgets import ReCaptcha
 from recaptcha.client import captcha
 
-from captcha.widgets import ReCaptcha
-
-
 class ReCaptchaField(forms.CharField):
-
     default_error_messages = {
         'captcha_invalid': _(u'Invalid captcha')
     }
@@ -23,10 +20,8 @@ class ReCaptchaField(forms.CharField):
         super(ReCaptchaField, self).clean(values[1])
         recaptcha_challenge_value = smart_unicode(values[0])
         recaptcha_response_value = smart_unicode(values[1])
-        check_captcha = captcha.submit(recaptcha_challenge_value,
+        check_captcha = captcha.submit(recaptcha_challenge_value, 
             recaptcha_response_value, settings.RECAPTCHA_PRIVATE_KEY, {})
         if not check_captcha.is_valid:
-            raise forms.util.ValidationError(
-                    self.error_messages['captcha_invalid'])
+            raise forms.util.ValidationError(self.error_messages['captcha_invalid'])
         return values[0]
-
