9b1052692acc28187e47d8460c287bbf2f3c44e9
Łukasz Kidziński <lukasz.kidzinski@gmail.com>
2011-09-08 11:20:56 +0200

    setup.py now downloads from mldata.org

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@
 import tarfile
 import os
 
-src = "http://lemonet.eu/static/"
-out_dir = "media/"
+src = "http://mldata.org/repository/data/download/%s/"
+out_dir = "media/private/data/"
 file_name = "private.tar.gz"
 
-def download(url):
+file_in = ["abalone", "book-crossing-ratings-10", "fish_killer", "regression-datasets-cpu_small"]
+file_out = ["abalone", "book-crossing-ratings", "fish-killer", "regression-cpu-small"]
+
+def download(url, localfile):
     """Copy the contents of a file from a given URL
     to a local file.
     """
     import urllib
-    webFile = urllib.urlopen(url)
-    localFile = open(out_dir + file_name, 'w')
+    webFile = urllib.urlopen(src % (url,))
+    localFile = open(out_dir + localfile + ".h5", 'w')
     localFile.write(webFile.read())
     webFile.close()
     localFile.close()
@@ -23,19 +26,15 @@ def install():
     print 'Loading fixtures... '
     os.system("python manage.py loaddata `find ./ -name '*.json'`")
 
-    try:
-        print 'Downloading %s... ' % (file_name,)
-        download(src + file_name)
-    except IOError:
-        print "IOError"
-        return
+    for i in range(0,len(file_in)):
+        try:
+            file_name = file_in[i]
+            print 'Downloading %s... ' % (file_name,)
+            download(file_name,file_out[i])
+        except IOError:
+            print "IOError"
+            return
         
-    print 'Extracting %s... ' % (file_name,)
-    tfile = tarfile.open(out_dir + file_name, 'r:gz')
-    tfile.extractall('media')
-    print 'Removing %s... ' % (file_name,)
-    os.remove(out_dir + file_name)
-
     print "Done!"
     print "Type 'python manage.py runserver' to start!"
 
