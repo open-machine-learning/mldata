171931ef7183fee4d976d53951826be128ced515
Łukasz Kidziński <lukasz.kidzinski@gmail.com>
2011-09-28 00:30:32 +0200

    Nice challenge viewer

--- challengeviewer/views.py
+++ challengeviewer/views.py
@@ -5,9 +5,10 @@ in the xml format.
 from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseRedirect
 from repository.models.data import Data
 from repository.models.challenge import Challenge
+from repository.models.task import Task
 
 def challengeviewer_index(request, slug):
     """
@@ -15,41 +16,57 @@ def challengeviewer_index(request, slug):
     """
     challenge = Challenge.get_object(slug)
     
-    return render_to_response('challengeviewer/base.html',
-                              {'challenge': challenge})
+    return render_to_response('challengeviewer/index.html',
+                              RequestContext(request,{'challenge': challenge}))
 
 def challengeviewer_results(request, slug):
     """
-        View displays main information about challenge
+        View displays results for given challenge
     """
     challenge = Challenge.get_object(slug)
 
     return render_to_response('challengeviewer/results.html',
-                              {'challenge': challenge})
+                              RequestContext(request,{'challenge': challenge}))
 
 def challengeviewer_task(request, slug, task_slug):
     """
-        View displays main information about challenge
+        View displays task details
     """
     challenge = Challenge.get_object(slug)
+    task = Task.get_object(task_slug)
 
     return render_to_response('challengeviewer/task.html',
-                              {'challenge': challenge})
+                              RequestContext(request,{'challenge': challenge,
+                               'task': task}))
 
 def challengeviewer_submit(request, slug):
     """
-        View displays main information about challenge
+        View allows user to submit results
     """
     challenge = Challenge.get_object(slug)
+    
+    from repository.views.base import handle_result_form
+    form = handle_result_form(request)
+    if form.added:
+        return HttpResponseRedirect(reverse('challengeviewer_results', args=[slug]))
 
     return render_to_response('challengeviewer/submit.html',
-                              {'challenge': challenge})
+                              RequestContext(request,{'challenge': challenge,
+                                                      'form': form}))
 
 def challengeviewer_login(request, slug):
     """
-        View displays main information about challenge
+        View displays and handles login form
     """
+    from django.contrib.auth import views as auth_views
     challenge = Challenge.get_object(slug)
+    return auth_views.login(request, template_name='challengeviewer/login.html',
+                            extra_context={'challenge': challenge, 'request': request})
 
-    return render_to_response('challengeviewer/login.html',
-                              {'challenge': challenge})
+def challengeviewer_logout(request, slug):
+    """
+        View logouts user from current request
+    """
+    from django.contrib.auth import logout
+    logout(request)
+    return HttpResponseRedirect(reverse('challengeviewer_index', args=[slug]))
